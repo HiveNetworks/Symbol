8b17ff991e44ad8bcbc75794529574f9c9ae733b
Asi Givati <asigivati@bzq-84-111-83-98.red.bezeqint.net>
2014-09-20 22:52:25 +0300

    first commit

---
+++ Symbol/Symbol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Symbol
+//
+//  Created by Asi Givati on 9/20/14.
+//  Copyright (c) 2014 Hive-Networks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Symbol/Symbol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Symbol
+//
+//  Created by Asi Givati on 9/20/14.
+//  Copyright (c) 2014 Hive-Networks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
